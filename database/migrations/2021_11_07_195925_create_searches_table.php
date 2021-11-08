fc085a7079ef913439aa9a988068c9b0c74a46e3
Benjamin Russell <benjamin.russell@lendio.com>
2021-11-07 17:05:04 -0700

    Add Search model and migration

---
+++ database/migrations/2021_11_07_195925_create_searches_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Frequency;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSearchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('searches', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Frequency::class);
+            $table->string('search_string', 50);
+            $table->json('results');
+            $table->timestamp('last_searched');
+            $table->unsignedTinyInteger('sleep_minutes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('searches');
+    }
+}
