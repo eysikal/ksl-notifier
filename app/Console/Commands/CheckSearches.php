64c82c57b8fe80d235b04bde552bca43c560e832
Benjamin Russell <benjamin.russell@lendio.com>
2021-11-28 16:16:12 -0700

    Add command for triggering search jobs

---
+++ app/Console/Commands/CheckSearches.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\PerformSearch;
+use App\Models\Search;
+use Illuminate\Console\Command;
+
+class CheckSearches extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'schedule:check-searches';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Loop through searches and perfom those that need it.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $searchesWithUserAndFrequency = Search::with(['user', 'frequency'])
+            ->whereRaw('NOW() >= next_search');
+
+        $searchesWithUserAndFrequency->each(function ($searchWithUserAndFrequency) {
+            PerformSearch::dispatch($searchWithUserAndFrequency);
+        });
+
+        return 0;
+    }
+}
